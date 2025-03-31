7faa65f68a95f4d30412b421704140e66eb003a0
TomiLT2005 <tomilopeztorres20@gmail.com>
2025-03-31 12:14:22 -0300

    Mensajes de cambios

--- PryLaboratorioProgramacion/Program.cs
+++ PryLaboratorioProgramacion/Program.cs
@@ -11,6 +11,8 @@ namespace PryLaboratorioProgramacion
         static void Main(string[] args)
         {
             Console.WriteLine("¡Lindo viernes para estar con Visual Studio 2022!");
+            Console.WriteLine("Cambios en el código");
+            Console.WriteLine("Otros Cambias");
         }
     }
 }
